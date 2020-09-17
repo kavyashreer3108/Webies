200c07ac9a9489d32d244cf4f94f4a4cf505a161
kavyashreer3108 <kavyashreer3108@gmail.com>
2020-09-17 15:28:59 +0530

    my first commit website

--- src/components/pages/Footer.js/Footer.js
+++ src/components/pages/Footer.js/Footer.js
@@ -73,7 +73,7 @@ function Footer() {
           <div className="footer-logo">
             <Link to="/" className="social-logo">
               <MdFingerprint className="navbar-icon" />
-              LAVISH
+              WEBIES
             </Link>
           </div>
           <small className="website-rights">
